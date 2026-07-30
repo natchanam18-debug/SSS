48ee1af653f8b1296276681a97fe35391d7f617a
natchanam18-debug <natchanam18@gmail.com>
2026-07-30 23:42:18 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,8 +1,7 @@
 $folder = "$env:TEMP\Hadestoolv2"
 if (!(Test-Path $folder)) { New-Item -ItemType Directory -Path $folder | Out-Null }
 
-# ใช้ลิงก์ Raw ที่ถูกต้องสำหรับดึงไฟล์ .zip โดยตรง
-$zipUrl = "https://github.com/natchanam18-debug/SSS/raw/main/app.zip"
+$zipUrl = "https://raw.githubusercontent.com/natchanam18-debug/SSS/refs/heads/main/app.zip"
 $zipPath = "$env:TEMP\app.zip"
 
 Write-Host "กำลังดาวน์โหลดไฟล์..." -ForegroundColor Cyan
