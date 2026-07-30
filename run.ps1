802ec5294b20defe1115bd20fa61855dc84a950e
natchanam18-debug <natchanam18@gmail.com>
2026-07-30 23:38:20 +0700

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,14 @@
+$folder = "$env:TEMP\Hadestoolv2"
+if (!(Test-Path $folder)) { New-Item -ItemType Directory -Path $folder | Out-Null }
+
+$zipUrl = "https://raw.githubusercontent.com/natchanam18-debug/my-first-project/refs/heads/main/app.zip"
+$zipPath = "$env:TEMP\app.zip"
+
+Write-Host "กำลังดาวน์โหลดไฟล์..." -ForegroundColor Cyan
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+
+Write-Host "กำลังแตกไฟล์..." -ForegroundColor Cyan
+Expand-Archive -Path $zipPath -DestinationPath $folder -Force
+
+Write-Host "กำลังเปิดโปรแกรม..." -ForegroundColor Green
+Start-Process "$folder\Hadestoolv2.exe"
